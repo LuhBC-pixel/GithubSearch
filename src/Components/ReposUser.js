2dcad0c6263e759303a84ab6d38d55351c1b4c5f
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-11-26 14:12:45 -0300

    finish

--- src/Components/ReposUser.js
+++ src/Components/ReposUser.js
@@ -20,33 +20,35 @@ function ReposUser(props) {
     getRepos();
   }, [setRepositories, reposUser]);
 
-  console.log(repositories);
-
   return (
     <div class='grid grid-cols-3 gap-4 mt-20 ml-40'>
       {repositories.map((repo) => (
-        <div class='bg-blackVeryDark w-96 flex'>
-          <div class='ml-5 mr-5' key={repo.id}>
-            <p class='text-gray font-display font-bold mt-4 text-xl'>{repo.name}</p>
+        <div class='bg-blackVeryDark w-96 flex flex-col'>
+          <div class='flex-1 ml-4'>
+            <p class='text-gray font-display font-bold mt-4 text-xl'>
+              {repo.name}
+            </p>
             <p class='text-gray'>
               {repo.description || 'No description completed'}
             </p>
-            <div class='h-20 flex items-end'>
-              <div class='flex mr-4'>
-                <img src={Star} alt='star icon' class='w-4 mr-2' />
-                <p class='text-gray'>{repo.stargazers_count || '0'}</p>
-              </div>
-              <div class='flex mr-4'>
-                <img src={Fork} alt='fork icon' class='w-4 mr-2' />
-                <p class='text-gray'>{repo.forks_count}</p>
-              </div>
-              <div class='flex mr-4'>
-                <img src={Language} alt='language icon' class='mr-2' />
-                <p class='text-gray'>{repo.language || 'Language'}</p>
-              </div>
-              <div class='flex mr-4'>
-                <a href={repo.html_url} target='_blank'><img src={Share} alt="share icon" class='w-4'/></a>
-              </div>
+          </div>
+          <div class='h-20 flex items-end ml-4 mb-4'>
+            <div class='flex mr-4'>
+              <img src={Star} alt='star icon' class='w-4 mr-2' />
+              <p class='text-gray'>{repo.stargazers_count || '0'}</p>
+            </div>
+            <div class='flex mr-4'>
+              <img src={Fork} alt='fork icon' class='w-4 mr-2' />
+              <p class='text-gray'>{repo.forks_count}</p>
+            </div>
+            <div class='flex mr-4'>
+              <img src={Language} alt='language icon' class='mr-2' />
+              <p class='text-gray'>{repo.language || 'x language'}</p>
+            </div>
+            <div class='flex mr-4'>
+              <a href={repo.html_url} target='_blank' rel='noreferrer'>
+                <img src={Share} alt='share icon' class='w-4' />
+              </a>
             </div>
           </div>
         </div>
