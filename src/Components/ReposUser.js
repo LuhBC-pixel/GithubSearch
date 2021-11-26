2ae626bf19099c694932df6789a1bd82b0ad530b
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-11-26 10:25:08 -0300

    show data of repos user

--- src/Components/ReposUser.js
+++ src/Components/ReposUser.js
@@ -1,37 +1,48 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import Star from '../assets/star.svg';
+import Fork from '../assets/code-branch-solid.svg';
+import language from '../assets/language.svg';
 
 function ReposUser(props) {
   const [repositories, setRepositories] = useState([]);
   const reposUser = props.repos.repos_url;
 
-  const getRepos = async () => {
-    const response = await fetch(`${reposUser}`);
-    const data = await response.json();
-    console.log(data);
+  useEffect(() => {
+    const getRepos = async () => {
+      const response = await fetch(`${reposUser}`);
+      const data = await response.json();
 
-    setRepositories(r => [
-      {
-        id: data.id,
-        name: data.name,
-        description: data.description,
-        language: data.language,
-        star: data.stargazers_count,
-        forks: data.forks,
-      },
-    ]);
-  };
+      setRepositories(data);
+    };
 
-  useCallback(getRepos, [ reposUser ]);
+    getRepos();
+  }, [setRepositories, reposUser]);
+
+  console.log(repositories);
 
   return (
-    <div class='bg-blackVeryDark w-96 h-20'>
+    <div class='grid grid-cols-3 gap-4 mt-20'>
       {repositories.map((repo) => (
-        <div class='mt-6 mb-2' key={repo.id}>
-          <p class='text-white text-center'>{repo.name}</p>
-          <p class='text-white'>{repo.description}</p>
-          <p class='text-white'>{repo.stars}</p>
-          <p class='text-white'>{repo.forks}</p>
-          <p class='text-white'>{repo.language}</p>
+        <div class='bg-blackVeryDark w-96 flex'>
+          <div class='mt-6 mb-2' key={repo.id}>
+            <p class='text-gray text-center'>{repo.name}</p>
+            <p class='text-gray'>{repo.description || 'No description completed'}</p>
+            <div class='h-20 flex content-end'>
+              <div class='flex'>
+                <img src={Star} alt='star icon' class='w-4' />
+                <p class='text-gray'>{repo.stars || '0'}</p>
+              </div>
+              <div class='flex'>
+                <img src={Fork} alt='fork icon' class='w-4' />
+                <p class='text-gray'>{repo.forks}</p>
+              </div>
+              <div class='flex'>
+                <img src={language} alt='language icon' />
+                <p class='text-gray'>{repo.language}</p>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
